610d11899d1e529d1877ee70252c5e540d26e288
Dmitry Mozgin <m0391n@gmail.com>
2024-07-28 05:37:44 +0300

    Update

---
+++ Assets/Scripts/Blackboard/BlackboardTester.cs
@@ -0,0 +1,28 @@
+using m039.Common.Blackboard;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Blackboard
+{
+    public class BlackboardTester : MonoBehaviour
+    {
+        m039.Common.Blackboard.Blackboard blackboard = new();
+
+        [SerializeField]
+        BlackboardData _BlackboardData;
+
+        private void Awake()
+        {
+            blackboard.SetValues(_BlackboardData);
+            var key22 = blackboard.GetOrRegisterKey("key22");
+            if (blackboard.TryGetValue(key22, out int key22Value))
+            {
+                Debug.Log("key22 => " + key22Value);
+            } else
+            {
+                Debug.Log("Can't find value.");
+            }
+        }
+    }
+}

1c4b8e3237ecf71581daa4849e1f0048d4b21808
Dmitry Mozgin <m0391n@gmail.com>
2024-08-17 15:23:22 +0300

    Update

--- Assets/Projects/StateMachineSample/Scripts/StateMachineSample/BotBrain.cs
+++ Assets/Projects/StateMachineSample/Scripts/StateMachineSample/BotBrain.cs
@@ -14,9 +14,18 @@ namespace Game.StateMachineSample
 
         [SerializeField] PatrolBotState _PatrolState;
 
+        [SerializeField]
+        TMPro.TMP_Text _Info;
+
         #endregion
 
-        StateMachine StateMachine { get; } = new();
+        StateMachine _stateMachine { get; } = new();
+
+        bool _idleClicked;
+
+        bool _patrolClicked;
+
+        bool _wanderClicked;
 
         public override void Init(CoreBotController botController)
         {
@@ -31,15 +40,47 @@ namespace Game.StateMachineSample
             var patrolState = _PatrolState;
             var wanderState = _WanderState;
 
-            StateMachine.AddAnyTransition(idleState, () => Input.GetKeyDown(KeyCode.R));
-            StateMachine.AddAnyTransition(patrolState, () => Input.GetKeyDown(KeyCode.P));
-            StateMachine.AddAnyTransition(wanderState, () => Input.GetKeyDown(KeyCode.W));
-            StateMachine.SetState(idleState);
+            _stateMachine.AddAnyTransition(idleState, () => Input.GetKeyDown(KeyCode.R) || _idleClicked);
+            _stateMachine.AddAnyTransition(patrolState, () => Input.GetKeyDown(KeyCode.P) || _patrolClicked);
+            _stateMachine.AddAnyTransition(wanderState, () => Input.GetKeyDown(KeyCode.W) || _wanderClicked);
+            _stateMachine.SetState(idleState);
         }
 
-        void OnGUI()
+        public void OnIdleClicked()
         {
-            if (StateMachine.CurrentState is MonoBehaviourState state)
+            _idleClicked = true;
+        }
+
+        public void OnPatrolClicked()
+        {
+            _patrolClicked = true;
+        }
+
+        public void OnWanderClicked()
+        {
+            _wanderClicked = true;
+        }
+
+        public override void Think()
+        {
+            _stateMachine.Update();
+            UpdateInfo();
+
+            _idleClicked = false;
+            _patrolClicked = false;
+            _wanderClicked = false;
+        }
+
+        public override void FixedThink()
+        {
+            base.FixedThink();
+
+            _stateMachine.FixedUpdate();
+        }
+
+        void UpdateInfo()
+        {
+            if (_stateMachine.CurrentState is MonoBehaviourState state)
             {
                 string getName(IState state)
                 {
@@ -53,20 +94,12 @@ namespace Game.StateMachineSample
                     }
                 }
 
-                GUI.Label(new Rect(10, 10, 1000, 200), "State: " + string.Join(" => ", state.GetHierarchicalStates().Select(getName)));
+                _Info.text = "State: " + string.Join(" => ", state.GetHierarchicalStates().Select(getName));
+                _Info.gameObject.SetActive(true);
+            } else
+            {
+                _Info.gameObject.SetActive(false);
             }
-        }
-
-        public override void Think()
-        {
-            StateMachine.Update();
-        }
-
-        public override void FixedThink()
-        {
-            base.FixedThink();
-
-            StateMachine.FixedUpdate();
         }
     }
 }

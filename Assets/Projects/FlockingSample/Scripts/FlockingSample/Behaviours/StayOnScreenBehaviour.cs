1b02a5502629e83624afe175f80c464a4bd9af31
Dmitry Mozgin <m0391n@gmail.com>
2024-08-26 23:52:02 +0300

    Change logic in StayOnScreenBehaviour

--- Assets/Projects/FlockingSample/Scripts/FlockingSample/Behaviours/StayOnScreenBehaviour.cs
+++ Assets/Projects/FlockingSample/Scripts/FlockingSample/Behaviours/StayOnScreenBehaviour.cs
@@ -7,18 +7,36 @@ namespace Game.FlockingSample
     [CreateAssetMenu(fileName = "StayOnScreenBehaviour", menuName = Consts.RootMenu + "/Flocking/StayOnScreen", order = 1)]
     public class StayOnScreenBehaviour : FlockingBehaviour
     {
+        [Range(0f, 1f)]
+        [SerializeField]
+        float _padding = 0f;
+
         public override Vector2 CalculateMove(FlockingManager manager, FlockingAgent agent)
         {
+            var move = Vector2.zero;
+            float t;
+
             var rect = CameraUtils.ScreenRect;
-            var radius = Mathf.Min(rect.width, rect.height);
-            var centerOffset = rect.center - agent.position;
-            var t = centerOffset.magnitude / radius;
-            if (t < 0.9f)
+            var size = Mathf.Min(rect.width, rect.height) * _padding / 2f;
+            var halfWidth = rect.width / 2f;
+            var halfHeight = rect.height / 2f;
+
+            var dx = agent.position.x - rect.center.x;
+            
+            if (Mathf.Abs(dx) > halfWidth - size)
             {
-                return Vector2.zero;
+                t = Mathf.Abs(dx) / halfWidth;
+                move.x -= dx * t * t;
             }
 
-            return centerOffset * t * t;
+            var dy = agent.position.y - rect.center.y;
+            if (Mathf.Abs(dy) > halfHeight - size)
+            {
+                t = Mathf.Abs(dy) / halfHeight;
+                move.y -= dy * t * t;
+            }
+            
+            return move;
         }
     }
 }
